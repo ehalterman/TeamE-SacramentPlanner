3c0abf9e21e53ae43275fc4638ef1fa7038685ab
Luiz Manoel <luiz.costa@magazineluiza.com.br>
2020-12-17 02:11:48 -0300

    Add Filter and sort in Speakers Create view

--- TeamE-SacramentPlanner/Pages/Speakers/Index.cshtml.cs
+++ TeamE-SacramentPlanner/Pages/Speakers/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using TeamE_SacramentPlanner.Data;
 using TeamE_SacramentPlanner.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 
 namespace TeamE_SacramentPlanner.Pages.Speakers
 {
@@ -22,10 +24,54 @@ namespace TeamE_SacramentPlanner.Pages.Speakers
         public IList<Speaker> Speaker { get;set; }
         public IList<MeetingProgram> Meetings { get; set; }
 
-        public async Task OnGetAsync()
+        public string BookSort { get; set; }
+        
+        public SelectList Notes { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BookSSelect { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            Speaker = await _context.Speaker.ToListAsync();
+
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+
+            
+            IQueryable<string> noteQuery = from m in _context.MeetingProgram
+                                           orderby m.MeetingDate
+                                           select m.MeetingDate.ToShortDateString();
+           
+            var books = from m in _context.Speaker select m;
+
+            var meets = from x in _context.MeetingProgram select x;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(s => s.SpeakerName.Contains(SearchString));
+            }
+
+             if (!string.IsNullOrEmpty(BookSSelect))
+            {
+                //////////// Here needs works ////////
+                //books = books.Where(x => x.MeetingID == 23);
+            }
+            // Sort
+             switch (sortOrder)
+            {
+                case "book_desc":
+                    books = books.OrderByDescending(s => s.SpeakerName);
+                    break;
+                default:
+                    books = books.OrderBy(s => s.SpeakerName);
+                    break;
+            }
+
+            Speaker = await books.AsNoTracking().ToListAsync();
             Meetings = await _context.MeetingProgram.ToListAsync();
+            Notes = new SelectList(await noteQuery.Distinct().ToListAsync());
         }
     }
 }
